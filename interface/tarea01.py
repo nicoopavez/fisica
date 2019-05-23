ff3f747a8c6e2bed25ee1678c9e6b680bef24f15
BorisAndresLmb <38543926+BorisAndresLmb@users.noreply.github.com>
2019-05-23 18:27:24 -0400

    Añadir nombre de curva en gráfica (#116)
    * Hippopede
    
    * Hippopede
    
    * Añadir Función Hipopoda
    
    * Añadir Función Hipopoda (Con documentación)
    
    * Añadir Función Hipopoda (Con doc
    
    * Añadir Función Hipopoda (Con docstring)
    
    * Borrar archivos basura
    
    * AÑADIR FUNCION HIPOPODA
    
    * Añadir nombre de la curva

--- interface/tarea01.py
+++ interface/tarea01.py
@@ -111,11 +111,9 @@ def hipopoda_1():
            return: plot Curve (Hipopede)
            '''
 
-    plt.rcParams['legend.fontsize'] = 10
-
+    plt.rcParams['legend.fontsize'] = 12
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-
     # Prepare arrays x, y, z
     theta = np.linspace(-4 * np.pi, 4 * np.pi, 99)
     a = 1
@@ -123,7 +121,7 @@ def hipopoda_1():
     x = a + (r - a) * np.cos(theta)
     y = (r - a) * np.sin(theta)
     z = 2 * (a * (r - a)) ** (1 / 2) * np.sin(theta / 2)
-    ax.plot(x, y, z, label='parametric curve')
+    ax.plot(x, y, z, label='Hipopede de Eudoxo')
 
     ax.legend()
 
