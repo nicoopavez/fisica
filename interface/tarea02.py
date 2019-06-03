e27e595c63cd0bd466c0fcc1312d1614fd6d081d
Omar Francisco Olivares <ofjolivares@gmail.com>
2019-06-03 16:28:39 -0400

    Añadir archivo de Tarea 2 (15%) #128

---
+++ interface/tarea02.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Hélice Conica
+
+# Hélice Circular
+
+# Corona Sinusoidal
+
+# Curva de Viviani
+
+# Hipopoda
+
+# Espiral Cónica de Papus
+
+# Curva de Arquitas
+
+# Horóptera
+
+# Curva Bicilindrica
+
+if __name__ == '__main__':
+    # Creación de Ventanas
+    root = tk.Tk()
+    root.wm_title("Tarea 02 (15%)")
+    root.geometry("800x600")
+
+    # Crear frame contenedor de los elementos
+    frame = tk.Frame(root)
+    frame.pack(padx=20, pady=20)
+    # Añadir titulo
+    label = tk.Label(frame, text="Curvas Paramétricas Famosas", height="2")
+    label.pack(fill=tk.X, expand=1)
